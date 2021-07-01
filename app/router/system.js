85a163a9e3ae3cb7a743aa668f71000c10542d04
iheeleme <iheeleme@gmail.com>
2021-07-01 23:27:07 +0800

    fix 路由地址

--- app/router/system.js
+++ app/router/system.js
@@ -2,17 +2,17 @@ module.exports = (app) => {
 	const { router, controller } = app;
 	const jwt = app.middleware.auth(app.config.jwt);
 
-	router.resources('getTree', '/menuManagement/getTree',jwt, controller.system.role);
-	router.resources('getList', '/roleManagement/getList',jwt, controller.system.role);
+	router.resources('getTree', '/api/menuManagement/getTree',jwt, controller.system.role);
+	router.resources('getList', '/api/roleManagement/getList',jwt, controller.system.role);
 	// router.post("/roleManagement/getList", jwt, controller.system.roleList);
 	// router.post("/menuManagement/getTree", jwt, controller.system.role);
 
 	// menu
-	router.resources('menuList', '/menuManagement/getList',jwt, controller.system.menu);
-	router.resources('getMenu', '/menuManagement/getMenu',jwt, controller.system.menu);
-	router.resources('menuDoDelete', '/menuManagement/doDelete',jwt, controller.system.menu);
-	router.resources('menuDoEdit', '/menuManagement/doEdit',jwt, controller.system.menu);
-	router.resources('menuDoAdd', '/menuManagement/doAdd',jwt, controller.system.menu);
+	router.resources('menuList', '/api/menuManagement/getList',jwt, controller.system.menu);
+	router.resources('getMenu', '/api/menuManagement/getMenu',jwt, controller.system.menu);
+	router.resources('menuDoDelete', '/api/menuManagement/doDelete',jwt, controller.system.menu);
+	router.resources('menuDoEdit', '/api/menuManagement/doEdit',jwt, controller.system.menu);
+	router.resources('menuDoAdd', '/api/menuManagement/doAdd',jwt, controller.system.menu);
 	// router.post("/menuManagement/doDelete", jwt, controller.system.menuDoDelete);
 	// router.get("/menuManagement/getList", jwt, controller.system.menuList);
 	// router.post("/menuManagement/doEdit", jwt, controller.system.menuDoEdit);
@@ -20,34 +20,34 @@ module.exports = (app) => {
 
 	// user
 	// router.post("/userManagement/getList", jwt, controller.system.userList);
-	router.resources('userList', '/userManagement/getList',jwt, controller.system.user);
-	router.resources('userDoDelete', '/userManagement/doDelete',jwt, controller.system.user);
-	router.resources('userDoEdit', '/userManagement/doEdit',jwt, controller.system.user);
-	router.resources('userDoAdd', '/userManagement/doAdd',jwt, controller.system.user);
-	router.resources('resetUserPwd', '/userManagement/reset/',jwt, controller.system.user);
+	router.resources('userList', '/api/userManagement/getList',jwt, controller.system.user);
+	router.resources('userDoDelete', '/api/userManagement/doDelete',jwt, controller.system.user);
+	router.resources('userDoEdit', '/api/userManagement/doEdit',jwt, controller.system.user);
+	router.resources('userDoAdd', '/api/userManagement/doAdd',jwt, controller.system.user);
+	router.resources('resetUserPwd', '/api/userManagement/reset/',jwt, controller.system.user);
 
 
 
-	router.get("/getRouters", jwt, controller.system.getRouters);
-	router.post("/icon/getList", jwt, controller.system.iconList);
+	router.get("/api/getRouters", jwt, controller.system.getRouters);
+	router.post("/api/icon/getList", jwt, controller.system.iconList);
 
 	// noticeManage
-	router.resources('noticeList', '/noticeManagement/getList',jwt, controller.system.notice);
-	router.resources('noticeDoEdit', '/noticeManagement/doEdit',jwt, controller.system.notice);
-	router.resources('noticeDoDelete', '/noticeManagement/doDelete',jwt, controller.system.notice);
+	router.resources('noticeList', '/api/noticeManagement/getList',jwt, controller.system.notice);
+	router.resources('noticeDoEdit', '/api/noticeManagement/doEdit',jwt, controller.system.notice);
+	router.resources('noticeDoDelete', '/api/noticeManagement/doDelete',jwt, controller.system.notice);
 	// router.post("/noticeManagement/getList", jwt, controller.system.noticeList);
 	// router.post("/noticeManagement/doEdit", jwt, controller.system.noticeDoEdit);
 	// router.post("/noticeManagement/doDelete", jwt, controller.system.noticeDoDelete);
 
 	// deptManage
-	router.resources('deptList', '/deptManagement/getList',jwt, controller.system.dept);
-	router.resources('deptDoDelete', '/deptManagement/doDelete',jwt, controller.system.dept);
-	router.resources('deptDoEdit', '/deptManagement/doEdit',jwt, controller.system.dept);
-	router.resources('deptDoAdd', '/deptManagement/doAdd',jwt, controller.system.dept);
+	router.resources('deptList', '/api/deptManagement/getList',jwt, controller.system.dept);
+	router.resources('deptDoDelete', '/api/deptManagement/doDelete',jwt, controller.system.dept);
+	router.resources('deptDoEdit', '/api/deptManagement/doEdit',jwt, controller.system.dept);
+	router.resources('deptDoAdd', '/api/deptManagement/doAdd',jwt, controller.system.dept);
 
 	// router.post("/deptManagement/getList", jwt, controller.system.deptList);
 	// router.post("/deptManagement/doDelete", jwt, controller.system.deptDoDelete);
 
 	// dict
-	router.resources('dictType','/system/dict/type',jwt,controller.system.dict)
+	router.resources('dictType','/api/system/dict/type',jwt,controller.system.dict)
 };
--- app/router/user.js
+++ app/router/user.js
@@ -1,8 +1,8 @@
 module.exports = (app) => {
 	const { router, controller } = app;
   const jwt = app.middleware.auth(app.config.jwt);
-  router.post("/publicKey",controller.home.publicKey);
-  router.post("/login", controller.home.login);
-	router.post("/admin", jwt, controller.home.index);
-	router.post("/userInfo", jwt, controller.home.userInfo);
+  router.post("/api/publicKey",controller.home.publicKey);
+  router.post("/api/login", controller.home.login);
+	router.post("/api/admin", jwt, controller.home.index);
+	router.post("/api/userInfo", jwt, controller.home.userInfo);
 };
